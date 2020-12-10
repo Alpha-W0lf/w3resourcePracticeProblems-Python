636e13f5d7237aac413543d24654418f12205de5
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-10 16:37:35 -0600

    more exercises

---
+++ Basic Part 1 Exercises/basic part 1 exercise 2-1.py
@@ -0,0 +1,8 @@
+# Write a program to return/print the Python version you are using. DONE
+import sys
+import platform
+
+print(sys.version)
+print(platform.python_version())
+
+# Can also be done in command line using "python --version"
---
+++ Basic Part 1 Exercises/basic part 1 exercise 3-1.py
@@ -0,0 +1,7 @@
+# Write a program to display the current date and time.
+
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+print(datetime.tzinfo)
