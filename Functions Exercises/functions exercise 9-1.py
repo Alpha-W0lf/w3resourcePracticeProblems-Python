6c7625b806ff21a6d7f0802c24a79cb8b6ca1e74
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-09 23:46:18 -0600

    Create functions exercise 9-1.py

---
+++ Functions Exercises/functions exercise 9-1.py
@@ -0,0 +1,18 @@
+# Write a function that takes a number as a parameter and check if the number is prime or not.
+
+
+def find_prime(num):
+    print(num)
+    divisible = False
+
+    for x in range(2, (num-1)):
+        if num % x == 0:
+            divisible = True
+            print('Number is NOT PRIME!')
+            break
+        else:
+            continue
+    if not divisible:
+        print('Number IS PRIME!')
+
+find_prime(710)
