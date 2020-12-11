ad5ee0b5e12386a9a09b2c5fbb8e4c2b4825f014
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-10 21:43:23 -0600

    finished basic 6-1

---
+++ Basic Part 1 Exercises/basic part 1 exercise 6-1.py
@@ -0,0 +1,21 @@
+# Write a program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with
+# those numbers. DONE
+
+raw_seq = input('Enter a sequence of numbers separated by commas: ')
+
+# list_seq = list(raw_seq)
+# for x in list_seq:
+#     if x == ' ':
+#         list_seq.remove(x)
+#     elif x == ',':
+#         list_seq.remove(x)
+#     else:
+#         continue
+# tuple_seq = tuple(list_seq)
+# print(list_seq)
+# print(tuple_seq)
+
+list_seq = raw_seq.split(', ')
+tuple_seq = tuple(list_seq)
+print(list_seq)
+print(tuple_seq)
