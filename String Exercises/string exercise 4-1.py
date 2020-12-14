abf5be053872c65227e8141cd1dd9b6b1ed396ad
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-14 00:11:26 -0600

    Update string exercise 4-1.py

--- String Exercises/string exercise 4-1.py
+++ String Exercises/string exercise 4-1.py
@@ -1,9 +1,34 @@
 # Write a program to get a string from a given string where all occurrences of its first char have been exchanged to
-# '$', except the first occurrence of the char itself.
+# '$', except the first occurrence of the char itself. DONE
 # Ex. Sample String: 'restart'
 # Ex. Expected Result: 'resta$t'
 
-phrase = str(input('Enter a string: '))
+# phrase = str(input('Enter a string: '))
 
-phrase_dict = {}
+# for x in phrase:
+#     if x == phrase[:1]:
+#         x = '0'
+#     else:
+#         x = x
+#
+# print(phrase)
+# print(phrase[0])
 
+
+# def char_change(phrase):
+#     char = phrase[0]
+#     phrase = phrase.replace(char, '$')
+#     phrase = char + phrase[1:]
+#     print(phrase)
+#
+# char_change('restart')
+
+
+def char_change(phrase):
+    char = phrase[0]
+    phrase = phrase.replace(char, '$')
+    phrase = char + phrase[1:]
+    print(phrase)
+
+
+char_change('restart')
