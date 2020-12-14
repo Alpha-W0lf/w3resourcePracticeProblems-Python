85c91301f6c59087a49b24e2ba91fc270df76af1
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-13 23:57:02 -0600

    Create string exercise 4-1.py

---
+++ String Exercises/string exercise 4-1.py
@@ -0,0 +1,9 @@
+# Write a program to get a string from a given string where all occurrences of its first char have been exchanged to
+# '$', except the first occurrence of the char itself.
+# Ex. Sample String: 'restart'
+# Ex. Expected Result: 'resta$t'
+
+phrase = str(input('Enter a string: '))
+
+phrase_dict = {}
+
