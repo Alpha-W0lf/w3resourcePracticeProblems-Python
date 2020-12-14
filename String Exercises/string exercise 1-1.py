b30b0918ff5f0c0a2aa45d6fe9b1af96ab813bf6
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-13 22:51:53 -0600

    continuing string exercises

--- String Exercises/string exercise 1-1.py
+++ String Exercises/string exercise 1-1.py
@@ -1,3 +1,6 @@
-# Write a program to calculate the length of a string.
+# Write a program to calculate the length of a string. DONE
 
 phrase = str(input('Please enter a phrase: '))
+phrase_length = len(phrase)
+
+print('The length of your phrase is', phrase_length, 'characters.')
