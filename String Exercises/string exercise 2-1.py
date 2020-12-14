c2ae43d6a64478fdd0588e1bc9666154f7a3eda3
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-13 23:32:09 -0600

    Update string exercise 2-1.py

--- String Exercises/string exercise 2-1.py
+++ String Exercises/string exercise 2-1.py
@@ -1,3 +1,13 @@
 # Write a program to count the number of characters (character frequency) in a string.
 
 phrase = str(input('Enter a phrase: '))
+
+all_freq = {}
+
+for i in phrase:
+    if i in all_freq:
+        all_freq[i] += 1
+    else:
+        all_freq[i] = 1
+
+print (all_freq)
