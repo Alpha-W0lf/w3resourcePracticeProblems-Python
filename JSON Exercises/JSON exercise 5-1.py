4884756d08010047349b608f9589349e65f7da98
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-15 15:32:59 -0600

    Update JSON exercise 5-1.py

--- JSON Exercises/JSON exercise 5-1.py
+++ JSON Exercises/JSON exercise 5-1.py
@@ -1,13 +1,12 @@
-# Write a program to convert JSON encoded data into Python objects.
+# Write a program to convert JSON encoded data into Python objects. DONE
 
 import json
 
-json_obj = {
-    "workers" : [
-        {"firstname" : "Sarah", "lastname" : "Connor"},
-        {"firstname" : "Brian", "lastname" : "Williams"},
-        {"firstname" : "Todd", "lastname" : "Baker"}
-    ]
-}
+json_obj_1 = '{"workers":[{"firstname":"Sarah", "lastname":"Connor"},{"firstname":"Brian", "lastname":"Williams"},{"firstname":"Todd", "lastname":"Baker"}]}'
 
-py_obj = json.load(json_obj)
+json_obj = '{"name" : "John", "age" : 45, "city" : "Austin"}'
+
+py_obj_1 = json.loads(json_obj_1)
+py_obj = json.loads(json_obj)
+print(py_obj_1)
+print(py_obj)
