5ce81bd9eb654cefc9f4b6d570b2d8eb00bf0622
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-15 14:35:04 -0600

    Update JSON exercise 4-1.py

--- JSON Exercises/JSON exercise 4-1.py
+++ JSON Exercises/JSON exercise 4-1.py
@@ -1,6 +1,15 @@
 # Write a program to convert Python dictionary object (sort by key) to JSON data. Print the object members with
-# indent level 4.
+# indent level 4. DONE
+
+import json
 
 py_dict = {
-
+    "name" : "Paul",
+    "age" : "44",
+    "city" : "Austin",
+    "hobby" : "bike riding"
 }
+
+json_dict = json.dumps(py_dict, sort_keys=True, indent=4)
+
+print(json_dict)
