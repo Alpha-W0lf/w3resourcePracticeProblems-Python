f1014ec1a5b89c25bbcc8d98ebe59d67c95e12d9
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-14 22:15:40 -0600

    Update string exercise 8-1.py

--- String Exercises/string exercise 8-1.py
+++ String Exercises/string exercise 8-1.py
@@ -1,19 +1,36 @@
-# Write a Python function that takes a list of words and returns the length of the longest one. Go to the editor
+# Write a Python function that takes a list of words and returns the length of the longest one. Go to the editor DONE
 # Sample Output:
 # Longest word: Exercises
 # Length of the longest word: 9
 
+# given = ['list', 'are', 'festival', 'edm']
+#
+#
+# def return_longest(words):
+#     given_len = []
+#     for x in given:
+#         given_len.append(len(x))
+#         print(x)
+#     print(given_len)
+#     longest_value = 0
+#     for y in given_len:
+#         if y > longest_value:
+#             longest_value = y
+#             longest_index = given_len.index(y)
+#             print(longest_value)
+#             print(longest_index)
+#             final = given[longest_index]
+#         else:
+#             continue
+#     print('The longest word is ' + final + '.')
+#     print('Its length is ' + str(longest_value) + '.')
+#
+#
+# return_longest(given)
+
+# Clumsy/inefficient way ^^ Going to redo below.
+
 given = ['list', 'are', 'festival', 'edm']
-
-
-def return_longest(words):
-    given_len = []
-    for x in given:
-        given_len.append(len(x))
-        print(x)
-        print(given_len)
-
-
-# x is the actual element in the list. we need to return the index of the element to build given_len[]
-
-return_longest(given)
+given.sort(key=len)
+print('The longest word is \"' + given[-1] + '\".')
+print(given.len())
