4f10b36b3d8e7420d4859b4977c43da818dd441d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-14 20:26:45 -0600

    Update string exercise 7-1.py

--- String Exercises/string exercise 7-1.py
+++ String Exercises/string exercise 7-1.py
@@ -1,19 +1,28 @@
 # Write a program to find the first appearance of the substring 'not' and 'poor' from a given string,
-# if 'poor' follows the 'not', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string.
+# if 'poor' follows the 'not', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string. DONE
 # Sample String : 'The lyrics is not that poor!'
 # 'The lyrics is poor!'
 # Expected Result : 'The lyrics is good!'
 # 'The lyrics is poor!'
 
-# First, find substring 'not'. if no match, return unchanged string.
-# Second, find substring 'poor'. if no match, return unchanged string.
-# Third, if 'poor' is found after 'not, replace ('not'...'poor') with 'good'.
+# First, find substring 'not'. if no match, return unchanged string. DONE
+# Second, find substring 'poor'. if no match, return unchanged string. DONE
+# Third, if 'poor' is found after 'not, replace ('not'...'poor') with 'good'. DONE
 
-phrase1 = 'The lyrics is not that poor!\nThe lyrics is poor!'
+import re
+
+phrase1 = 'The lyrics is note that pore!\nThe lyrics is not poors!'
 
 
 def notpoortogood(phrase):
-    
+    if phrase.find('not'):
+        if phrase.find('poor'):
+            phrase = re.sub('(?s)not\W+.+poor\W*', 'good', phrase)
+            print(phrase)
+        else:
+            print(phrase)
+    else:
+        print(phrase)
 
 
 notpoortogood(phrase1)
