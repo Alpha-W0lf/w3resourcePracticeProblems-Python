040549a1f66aa390e31b755210a97d49ccee4c26
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-26 13:49:21 -0600

    Create basic part 1 exercise 4-3.py

---
+++ Basic Part 1 Exercises/basic part 1 exercise 4-3.py
@@ -0,0 +1,12 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area. Go to the editor
+# Sample Output :
+# r = 1.1
+# Area = 3.8013271108436504
+
+import math
+
+radius = float(input('Enter the radius of a circle: '))
+area = math.pi * (radius ** 2)
+area_rounded = ("%.2f" % area)
+
+print('The area of the circle is', area_rounded)
