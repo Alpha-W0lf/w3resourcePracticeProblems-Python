2b56fe7b92512b6662bcce0eeaca05d7bc168228
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-26 18:35:29 -0600

    Create basic part 1 exercise 8-3.py

---
+++ Basic Part 1 Exercises/basic part 1 exercise 8-3.py
@@ -0,0 +1,4 @@
+# Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+
+print('The first color is: '+color_list[0]+'\nThe last color is: '+color_list[-1])
