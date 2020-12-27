c20ad0fdc381c243f0d954264fb6fcb41959fb96
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-26 18:24:05 -0600

    Update basic part 1 exercise 6-3.py

--- Basic Part 1 Exercises/basic part 1 exercise 6-3.py
+++ Basic Part 1 Exercises/basic part 1 exercise 6-3.py
@@ -5,3 +5,16 @@
 # List : ['3', ' 5', ' 7', ' 23']
 # Tuple : ('3', ' 5', ' 7', ' 23')
 
+given = input('Please enter a list of numbers separated by commas: ')
+
+given_list_str = given.split(', ')
+given_list_int = list(map(int, given.split(', ')))
+given_tuple_str = tuple(given.split(', '))
+given_tuple_int = tuple(map(int, given.split(', ')))
+
+print(given_list_str)
+print(given_list_int)
+print(given_tuple_str)
+print(given_tuple_int)
+
+# DONE
