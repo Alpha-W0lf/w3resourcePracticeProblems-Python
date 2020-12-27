f2930331127e0d5e7490d539480b58d11a976108
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-26 19:01:14 -0600

    Update basic part 1 exercise 10-3.py

--- Basic Part 1 Exercises/basic part 1 exercise 10-3.py
+++ Basic Part 1 Exercises/basic part 1 exercise 10-3.py
@@ -2,3 +2,19 @@
 # Sample value of n is 5
 # Expected Result : 615
 
+n = int(input('Enter an integer: '))
+digits = int(input('Enter the number of digits for the answer: '))
+
+
+def strange_add(given, length):
+    count = 1
+    answer_list = []
+    given = str(given)
+
+    while count <= length:
+        answer_list.append(given*count)
+        count += 1
+    print(sum(answer_list))
+
+
+strange_add(n, digits)
