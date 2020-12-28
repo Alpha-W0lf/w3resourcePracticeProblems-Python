70dec313131c67702e5b82dd8cc48fc3e4517880
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-27 20:40:07 -0600

    Create string exercise 2-2.py

---
+++ String Exercises/string exercise 2-2.py
@@ -0,0 +1,8 @@
+# Write a Python program to count the number of characters (character frequency) in a string. Go to the editor
+# Sample String : google.com'
+# Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+given = str(input('Enter a string: '))
+
+given_dict = {}
+
