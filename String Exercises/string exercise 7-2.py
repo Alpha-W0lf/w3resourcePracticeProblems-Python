5941500d10e36a4b5c4bb9695805227833c9c2af
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-28 14:46:17 -0600

    Update string exercise 7-2.py

--- String Exercises/string exercise 7-2.py
+++ String Exercises/string exercise 7-2.py
@@ -1,8 +1,14 @@
 # Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string,
-# if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string.
+# if 'poor' follows the 'not', replace the whole 'not'...'poor' substring with 'good'. Return the
+# resulting string.
 # Go to the editor
 # Sample String : 'The lyrics is not that poor!'
 # 'The lyrics is poor!'
 # Expected Result : 'The lyrics is good!'
 # 'The lyrics is poor!'
 
+def sub_find(given):
+
+
+
+sub_find('The lyrics is not that poor!')
