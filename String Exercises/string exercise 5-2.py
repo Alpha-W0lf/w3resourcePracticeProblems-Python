d858f98260fbc0356f9302ae2fd2d1227dfc55b0
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-28 14:36:39 -0600

    Update string exercise 5-2.py

--- String Exercises/string exercise 5-2.py
+++ String Exercises/string exercise 5-2.py
@@ -1,3 +1,15 @@
-# Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string. Go to the editor
+# Write a Python program to get a single string from two given strings, separated by a space and swap the first two
+# characters of each string. Go to the editor
 # Sample String : 'abc', 'xyz'
 # Expected Result : 'xyc abz'
+
+given_1 = str(input('Enter a string: '))
+given_2 = str(input('Enter a second string: '))
+
+
+def char_swap(str_1, str_2):
+    answer = str_2[:2]+str_1[2:]+' '+str_1[:2]+str_2[2:]
+    print(answer)
+
+
+char_swap(given_1, given_2)
