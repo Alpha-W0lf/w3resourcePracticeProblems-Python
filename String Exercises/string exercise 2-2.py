6664d4f82560b07ac2b93e03112a6f0b724c72cc
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-27 20:48:59 -0600

    Update string exercise 2-2.py

--- String Exercises/string exercise 2-2.py
+++ String Exercises/string exercise 2-2.py
@@ -1,8 +1,31 @@
 # Write a Python program to count the number of characters (character frequency) in a string. Go to the editor
 # Sample String : google.com'
 # Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+#
+# given = str(input('Enter a string: '))
+#
+#
+# def char_count(string):
+#     given_dict = {}
+#     for x in string:
+#         keys = given_dict.keys()
+#         if x in keys:
+#             given_dict[x] += 1
+#         else:
+#             given_dict[x] = 1
+#     print(given_dict)
+#
+#
+# char_count(given)
 
-given = str(input('Enter a string: '))
 
-given_dict = {}
+given = str(input("Enter a string: "))
 
+def char_count(string):
+    given_dict = {}
+    for x in given_dict:
+        
+
+char_count(given)
+
+# DONE
