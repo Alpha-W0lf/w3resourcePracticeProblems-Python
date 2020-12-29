bfb76c1fcebb702da021d7e415a6e082dea31850
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-28 18:40:37 -0600

    Update string exercise 8-2.py

---
+++ String Exercises/string exercise 8-2.py
@@ -0,0 +1,5 @@
+# Write a Python function that takes a list of words and returns the length of the longest one. Go to the editor
+# Sample Output:
+# Longest word: Exercises
+# Length of the longest word: 9
+
