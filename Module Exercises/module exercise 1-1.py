d39f4aeb89eb83892a2d861d9bf02aec9aa4995d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-09 12:44:28 -0500

    Create module exercise 1-1.py

---
+++ Module Exercises/module exercise 1-1.py
@@ -0,0 +1,2 @@
+# Write a Python program to generate a random color hex, a random alphabetical string, random value between two
+# integers (inclusive) and a random multiple of 7 between 0 and 70. Use random.randint()
