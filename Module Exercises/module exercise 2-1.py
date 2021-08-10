8abd249a8a446caec36b9dcd9c088c5db4630e14
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-09 20:21:08 -0500

    Create module exercise 2-1.py

---
+++ Module Exercises/module exercise 2-1.py
@@ -0,0 +1,8 @@
+# Write a Python program to select a random element from a list, set, dictionary (value) and a file from a directory.
+# Use random.choice()
+
+import random
+
+given_list =
+given_set =
+given_dictionary =
