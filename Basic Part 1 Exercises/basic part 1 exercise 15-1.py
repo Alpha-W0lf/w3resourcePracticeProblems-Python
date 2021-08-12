ddebd94ba893fae3d5076de4d040e51fca82bf47
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-12 13:08:20 -0500

    Update basic part 1 exercise 15-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 15-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 15-1.py
@@ -1,2 +1,9 @@
 # Write a Python program to get the volume of a sphere with radius 6.
 
+import math
+
+r = float(input("Enter radius of sphere: "))
+
+volume = (4/3)*(math.pi)*(r**3)
+
+print("The volume of this sphere is:", volume)
