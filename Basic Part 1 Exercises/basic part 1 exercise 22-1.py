24868eaf09682a81e7352a610df0bd9030a24093
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-13 11:33:23 -0500

    Update basic part 1 exercise 22-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 22-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 22-1.py
@@ -1,2 +1,16 @@
 # Write a Python program to count the number 4 in a given list.
 
+given = input("Provide a list separated by spaces: ")
+
+given_list = given.split(" ")
+
+count = 0
+
+for i in given_list:
+    if i == "4":
+        count += 1
+    else:
+        continue
+
+print(given_list)
+print(count)
