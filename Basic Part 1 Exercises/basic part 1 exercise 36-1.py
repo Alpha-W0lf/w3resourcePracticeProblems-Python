82f3f90a198767758dc9839ae98e38b2667ae8fb
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 13:59:02 -0500

    Update basic part 1 exercise 36-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 36-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 36-1.py
@@ -1,9 +1,9 @@
 # Write a Python program to add two objects if both objects are an integer type.
 
-obj1 = 23
-obj2 = 33.0
+def check_type(obj1, obj2):
+    if type(obj1) and type(obj2) == int:
+        print(obj1 + obj2)
+    else:
+        print("These are not both integers.")
 
-if type(obj1) and type(obj2) == int:
-    print(obj1 + obj2)
-else:
-    print("These are not both integers.")
+check_type(23, -3.3)
