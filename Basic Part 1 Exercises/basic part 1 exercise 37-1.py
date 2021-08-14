7d67536fd4fa4abac8e41419d6acd0e1b451fb3d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 14:04:25 -0500

    Update basic part 1 exercise 37-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 37-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 37-1.py
@@ -1,2 +1,11 @@
 # Write a Python program to display your details like name, age, address in three different lines.
 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+address = input("Enter your address: ")
+
+print(name)
+print(age)
+print(address)
+
+print(name + "\n" + age + "\n" + address)
