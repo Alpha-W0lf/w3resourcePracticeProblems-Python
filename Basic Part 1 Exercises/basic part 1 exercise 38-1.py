be8989b047a56c149f50febd8d44a421e725d1bb
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 14:11:18 -0500

    Create basic part 1 exercise 38-1.py

---
+++ Basic Part 1 Exercises/basic part 1 exercise 38-1.py
@@ -0,0 +1,9 @@
+# Write a Python program to solve (x + y) * (x + y). Go to the editor
+# Test Data : x = 4, y = 3
+# Expected Output : (4 + 3) ^ 2) = 49
+
+def formula(x, y):
+    answer = (x + y) ** 2
+    print("(x + y) ^ 2 =", answer)
+
+formula(2, 3)
