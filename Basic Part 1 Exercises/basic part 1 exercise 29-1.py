e307262fb2384eeb5c918a55738c53e0629506e5
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 12:52:36 -0500

    Update basic part 1 exercise 29-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 29-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 29-1.py
@@ -9,15 +9,19 @@
 color_list_1 = set(["White", "Black", "Red"])
 color_list_2 = set(["Red", "Green"])
 
-answer = set([])
+# answer = set([])
+#
+# def compare(x, y):
+#     for i in x:
+#         if i in y:
+#             continue
+#         else:
+#             answer.add(i)
+#     print(answer)
+#
+#
+# compare(color_list_1, color_list_2)
 
-def compare(x, y):
-    for i in x:
-        if i in y:
-            continue
-        else:
-            answer.add(i)
-    print(answer)
+final = color_list_1 - color_list_2
 
-
-compare(color_list_1, color_list_2)
+print(final)
