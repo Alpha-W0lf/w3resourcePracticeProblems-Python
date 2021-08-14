129e68d4eb89dfe5a2f1f62474b4037b9cf7e263
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 13:48:02 -0500

    Update basic part 1 exercise 35-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 35-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 35-1.py
@@ -1,2 +1,5 @@
 # Write a Python program that will return true if the two given integer values are equal or their sum or difference is 5.
 
+x = int(input("Enter an integer: "))
+y = int(input("Enter another integer: "))
+
