0ac96a4739ed1f7740e5d894cb3c2e02f9f9b020
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-14 13:29:26 -0500

    Update basic part 1 exercise 31-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 31-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 31-1.py
@@ -1,5 +1,8 @@
 # Write a Python program to compute the greatest common divisor (GCD) of two positive integers.
 
+from math import gcd
+
 int1 = int(input("Enter a positive integer. (A negative integer will be converted to positive anyway): "))
 int2 = int(input("Enter another positive integer. (A negative integer will be converted to positive anyway): "))
 
+print(gcd(int1, int2))
