a39ba32c44801e2375eeec8508b71b6e3ff82614
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-15 16:41:55 -0500

    Update basic part 1 exercise 48-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 48-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 48-1.py
@@ -1,3 +1,9 @@
 # Write a Python program to parse a string to Float or Integer.
 
-given = 
+given = input("Enter a number: ")
+
+given = float(given)
+print(given, "is now a float.")
+
+given = int(given)
+print(given, "is now an integer.")
