062aef42a6fbdea4e353bcebbe3c062ba499109e
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-16 15:56:29 -0500

    Update basic part 1 exercise 59-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 59-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 59-1.py
@@ -1,3 +1,6 @@
 # Write a Python program to convert height (in feet and inches) to centimeters.
 
-given_height = input("Enter height in feet and inches (\' and \" : ")
+given_height = input("Enter height in feet and inches (\' and \") : ")
+given_list = given_height.split("\' ")
+
+print(given_list)
