9574f145aac4adf82f58ec55f2f2fb63e833176c
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-17 11:02:42 -0500

    Update basic part 1 exercise 60-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 60-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 60-1.py
@@ -2,3 +2,15 @@
 
 # a ** 2 + b ** 2 = c ** 2
 
+from math import sqrt
+
+height =  float(input("Enter the height of the triangle: "))
+length = float(input("Enter the length of the triangle: "))
+
+
+def triangle(x, y):
+    hypo = sqrt((height ** 2) + (length ** 2))
+    print(hypo)
+
+
+triangle(height, length)
