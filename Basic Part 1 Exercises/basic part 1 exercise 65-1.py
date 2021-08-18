d9a51d7507fa91ae58caad94f1a9f3874595b750
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-18 14:56:41 -0500

    Update basic part 1 exercise 65-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 65-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 65-1.py
@@ -1,2 +1,21 @@
 # Write a Python program to convert seconds to day, hour, minutes and seconds.
 
+import time
+
+given = int(input("Enter number of seconds: "))
+
+days = given / (3600 * 24)
+days = int(days)
+remainder = given % (3600 * 24)
+
+hours = remainder / (60 * 60)
+hours = int(hours)
+remainder = remainder % (60 * 60)
+
+minutes = remainder / 60
+minutes = int(minutes)
+remainder = remainder % 60
+
+seconds = remainder
+
+print("That number of seconds is equal to", days, "days,", hours, "hours,", minutes, "minutes, and", seconds, "seconds.")
