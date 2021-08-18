1b040d8367c9a473dc7e645d1ccd999d2ebe45c7
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-18 15:07:15 -0500

    Create basic part 1 exercise 66-1.py

---
+++ Basic Part 1 Exercises/basic part 1 exercise 66-1.py
@@ -0,0 +1,4 @@
+# Write a Python program to calculate body mass index.
+
+weight = float(input("Enter your weight in pounds: "))
+height = float(input("Enter your height in feet and inches using \(\' \"\) format: "))
