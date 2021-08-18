1e46909674a121c68f5d895f769aa9a8e6c076b3
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-18 15:19:08 -0500

    Update basic part 1 exercise 66-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 66-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 66-1.py
@@ -1,4 +1,19 @@
 # Write a Python program to calculate body mass index.
 
+# BMI formula: (weight / ((height) ** 2)) * 703
+
 weight = float(input("Enter your weight in pounds: "))
-height = float(input("Enter your height in feet and inches using \(\' \"\) format: "))
+height = input("Enter your height in feet and inches using (\' \") format: ")
+
+height_list = height.split("' ")
+height_list[1] = height_list[1].replace("\"","")
+
+height_list_int = list(map(int, height_list))
+
+print(height_list_int)
+height_inches = (height_list_int[0] * 12) + height_list_int[1]
+print(height_inches)
+
+BMI = (weight / ((height_inches) ** 2) * 703)
+
+print("BMI is:", BMI)
