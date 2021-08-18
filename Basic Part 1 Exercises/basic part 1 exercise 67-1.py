dce82fd083486db7ed60dc59d071055467fc0dd8
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-18 15:21:59 -0500

    Create basic part 1 exercise 67-1.py

---
+++ Basic Part 1 Exercises/basic part 1 exercise 67-1.py
@@ -0,0 +1,5 @@
+# Write a Python program to convert pressure in kilopascals to pounds per square inch, a millimeter of mercury (mmHg) and atmosphere pressure.
+
+# kilopascal to PSI: divide by 6.895
+# kilospascal to mmHg: multiply by 7.501
+# kilopascal to atmosphere pressure: divide by 101
