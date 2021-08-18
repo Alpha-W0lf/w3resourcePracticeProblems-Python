6d115242f298c34e34af4f8a63ad74dc4d90dbac
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-18 15:28:15 -0500

    Update basic part 1 exercise 67-1.py

--- Basic Part 1 Exercises/basic part 1 exercise 67-1.py
+++ Basic Part 1 Exercises/basic part 1 exercise 67-1.py
@@ -3,3 +3,13 @@
 # kilopascal to PSI: divide by 6.895
 # kilospascal to mmHg: multiply by 7.501
 # kilopascal to atmosphere pressure: divide by 101
+
+kilopascal = float(input("Enter pressure in kilopascals: "))
+
+
+PSI = kilopascal / 6.895
+mmHg = kilopascal * 7.501
+atm = kilopascal / 101
+
+print(kilopascal, PSI, mmHg, atm)
+print("%g kilopascals is equal to %s PSI, %g mmHg, and %f atm." % (kilopascal, PSI, mmHg, atm))
